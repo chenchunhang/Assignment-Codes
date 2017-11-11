25ef57aa72a3a7e56b2c4198e7606ca049f2fc99
acer <acer@acer-PC>
2017-11-11 20:18:04 +0800

    My Part(Staff)

---
+++ Assignment-Codes/src/entity/Staff.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.*;
+
+public class Staff {
+     String staffID;
+     String staffName;
+     int staffPhone;
+     String staffAddress;
+     int staffAge;
+    
+    public String getStaffID(){
+   return staffID;
+}
+    public void setStaffID( String staffID){
+    this.staffID=staffID;
+}
+    public String getStaffName(){
+        return staffName;
+    }
+    public void setStaffName(String staffName){
+        this.staffName=staffName;
+    }
+    public int getStaffPhone(){
+        return staffPhone;
+    }
+    public void setStaffPhone(int staffPhone){
+        this.staffPhone=staffPhone;
+    }
+    public String getStaffAddress(){
+        return staffAddress;
+    }
+    public void setStaffAddress(String Address){
+        this.staffAddress=Address;
+        
+    }
+    public int getStaffAge(){
+        return staffAge;
+    }
+    public void setStaffAge(int staffAge){
+        this.staffAge=staffAge;
+    }
+}
